0aeb8545e378e43c30e655f6e54abe205e9920ba
Om-Joshi07 <omjoshi424566@gmail.com>
2025-08-29 14:56:31 +0545

    improvedNavBar

--- port-folio/src/NavBar/NavBar.jsx
+++ port-folio/src/NavBar/NavBar.jsx
@@ -1,42 +1,34 @@
 
 
-import {React, useState} from 'react'
-import './NavBar.css'
-import { RxHamburgerMenu } from "react-icons/rx";
-
+import { useState } from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
-    const [showMenu, setShowMenu] = useState(false);
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    }
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <header>
-        <div className="container">
-            <div className="grid navbar-grid">
-                <div className="logo">
-                    <h1>Mero Portfolio</h1>
-                </div>
-                
-                <nav className={showMenu ? "menu-mobile": "menu-web"}>
-                    <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#skills">Services</a></li>
-                        <li><a href="#projects">Portfolio</a></li>
-                    </ul> 
-                </nav>
-                {/* Ham Part  */}
-                <div className="ham-menu">
-                    <button onClick={toggleMenu} >
-                    <RxHamburgerMenu />
-                    </button>
-                </div>
-            </div>
-        </div>
-    </header>
-  )
-}
+      <div className="container">
+        <div className="navbar-grid">
+          <nav className={isMenuOpen ? "menu-mobile" : "menu-web"}>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#skills">Services</a></li>
+              <li><a href="#projects">Portfolio</a></li>
+            </ul>
+          </nav>
 
-export default NavBar
+          {/* Hamburger Menu */}
+          <div className="ham-menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <div className={`ham-bar ${isMenuOpen ? "clicked" : ""}`}></div>
+            <div className={`ham-bar ${isMenuOpen ? "clicked" : ""}`}></div>
+            <div className={`ham-bar ${isMenuOpen ? "clicked" : ""}`}></div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
