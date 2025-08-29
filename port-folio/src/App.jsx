07d8e031ee0fe4cb58930676bec1abe12bf3d0f8
Om-Joshi07 <omjoshi424566@gmail.com>
2025-08-29 10:50:19 +0545

    madeNAVBar

--- port-folio/src/App.jsx
+++ port-folio/src/App.jsx
@@ -6,9 +6,11 @@ import NavBar from "./NavBar/NavBar"
 const App = () => {
     return (
         <>
-            <h1>Hello. This is my Portfolio TypeShiiiiiii</h1>
             <NavBar/>
 
         </>   
     )
 }
+
+
+export default App
--- port-folio/src/NavBar/NavBar.jsx
+++ port-folio/src/NavBar/NavBar.jsx
@@ -1,8 +1,16 @@
 
 
-import React from 'react'
+import {React, useState} from 'react'
+import './NavBar.css'
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 const NavBar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
   return (
     <header>
         <div className="container">
@@ -10,15 +18,21 @@ const NavBar = () => {
                 <div className="logo">
                     <h1>Mero Portfolio</h1>
                 </div>
-
-                <nav>
+                
+                <nav className={showMenu ? "menu-mobile": "menu-web"}>
                     <ul>
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#skills">Services</a></li>
                         <li><a href="#projects">Portfolio</a></li>
-                    </ul>
+                    </ul> 
                 </nav>
+                {/* Ham Part  */}
+                <div className="ham-menu">
+                    <button onClick={toggleMenu} >
+                    <RxHamburgerMenu />
+                    </button>
+                </div>
             </div>
         </div>
     </header>
