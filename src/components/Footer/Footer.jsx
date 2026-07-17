692e8c5e7748df27f6aca129e31bfe8173788deb
Om-Joshi07 <omjoshi424566@gmail.com>
2026-07-17 22:39:51 +0545

    pushed:Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { VscGithubInverted } from "react-icons/vsc";
-import { BsInstagram, BsStackOverflow } from "react-icons/bs";
-import { RxLinkedinLogo } from "react-icons/rx";
 import "./Footer.css";
 
 const Footer = () => {
@@ -9,36 +6,6 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="footer-grid">
-          <div className="social-icons">
-            <a
-              href="https://www.linkedin.com/in/om-joshi07/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <RxLinkedinLogo />
-            </a>
-            <a
-              href="https://github.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <VscGithubInverted />
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BsInstagram />
-            </a>
-            <a
-              href="https://stackoverflow.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <BsStackOverflow />
-            </a>
-          </div>
           <div className="text">
             <p>© 2024 Om Joshi. All rights reserved.</p>
             <p>Designed by Om Joshi</p>
